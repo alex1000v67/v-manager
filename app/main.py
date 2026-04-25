2f29a170e96df6561f0c91af9a54a947817de69b
alex1000v67 <belovaleesej@gmail.com>
2026-04-25 20:58:16 +0300

    Added Free Memory check and uptime check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+import subprocess
 logger = logging.getLogger(__name__)
 
 app = FastAPI(
@@ -11,6 +12,13 @@ app = FastAPI(
     docs_url="/docs",
 )
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+@app.get("/system/uptime")
+def uptime():
+    uptime = subprocess.run("uptime", shell=True, capture_output=True, text=True)
+    return {"output": uptime.stdout.strip()}
+
+@app.get("/system/freeozu")
+def freeozu():
+    ozu = subprocess.run("free -m", shell=True, capture_output=True, text=True)
+    return {"output": ozu.stdout.strip()}
+
